78bb90cd8a8cb74a953af624ecad306ffacd173c
Kevin Page <kpage@users.noreply.github.com>
2017-01-01 16:17:13 +0700

    Improving vendoring script
    * No more submodules
    
    It's possible this would be better improved by moving to the vendoring
    docker image instead of putting logic in a one-off script in each
    project directory.

--- vendor.sh
+++ vendor.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+rm -rf ./vendor/*
 docker run --rm -v $PWD:/app -w /app treeder/go vendor
 mv ./vendor/src/* ./vendor
 rm -rf ./vendor/src
 rm -rf ./vendor/pkg
+# By default this vendoring will add git submodules, we remove them
+# and just check in the files directly to our repo
+find ./vendor -type d -name .git -print0  | xargs -0 rm -rf
