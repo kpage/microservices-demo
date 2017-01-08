69b2eec93e929a24f3f1953c432b65a3c3b03c58
Kevin Page <kpage@users.noreply.github.com>
2017-01-08 14:54:11 +0700

    Move hello world to rest-api and add docker compose

--- rest-api/app.go
+++ rest-api/app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
---
+++ rest-api/installAndStart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /go/src/app
+#go-wrapper download
+#go-wrapper install
+#check and install gin if missing i.e. go directory was overriden or mounted
+#go get -v github.com/codegangsta/gin
+exec gin "$@"
